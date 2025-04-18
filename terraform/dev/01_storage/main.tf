0b74d6aa166ac99d640db57bee3ef4b91b1c2252
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-04-18 19:19:03 +0800

    feat: terraform fmt

--- terraform/dev/01_storage/main.tf
+++ terraform/dev/01_storage/main.tf
@@ -1,11 +1,11 @@
-    # terraform/dev/01_storage/main.tf
+# terraform/dev/01_storage/main.tf
 
-    terraform {
-      required_providers {
-        aws = {
-          source  = "hashicorp/aws"
-          version = "~> 5.0"
-        }
-      }
-      required_version = ">= 1.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
+  required_version = ">= 1.0"
+}
