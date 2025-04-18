ebad121af321c041f4f4cc09ae80fd5f63f67d7d
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-04-18 19:12:56 +0800

    feat: add storage layer

---
+++ terraform/dev/01_storage/main.tf
@@ -0,0 +1,10 @@
+    # terraform/dev/01_storage/main.tf
+
+    terraform {
+      required_providers {
+        aws = {
+          source  = "hashicorp/aws"
+          version = "~> 5.0"
+        }
+      }
+      required_version = ">= 1.0"
