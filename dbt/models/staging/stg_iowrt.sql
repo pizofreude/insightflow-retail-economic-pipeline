12a9c1df7b6784d304569be2650a30d13bda9cdb
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-04-20 04:40:34 +0800

    feat: add wholesale stg

---
+++ dbt/models/staging/stg_iowrt.sql
@@ -0,0 +1,24 @@
+-- models/staging/stg_iowrt.sql
+
+with source_data as (
+    -- Select data from the raw source table created by Glue Crawler
+    select
+        series,
+        "date", -- Quoted because date is a reserved keyword in some SQL dialects
+        sales,
+        volume,
+        volume_sa
+    from {{ source('landing_zone', 'iowrt') }}
+    -- Filter for absolute values as growth figures can be recalculated later if needed
+    where series = 'abs'
+)
+
+select
+    -- Cast data types and rename columns
+    cast("date" as date) as record_date,
+    cast(sales as double) as sales_value_rm_mil,
+    cast(volume as double) as volume_index,
+    cast(volume_sa as double) as volume_index_sa
+
+from source_data
+-- Optional: Add any other basic filtering or cleaning specific to this source
