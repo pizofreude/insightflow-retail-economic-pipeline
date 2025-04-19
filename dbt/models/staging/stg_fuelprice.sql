047be5a98d1f29d31b8b5445131b4ab750076112
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-04-20 04:39:07 +0800

    feat: add fuel price stg

---
+++ dbt/models/staging/stg_fuelprice.sql
@@ -0,0 +1,25 @@
+-- models/staging/stg_fuelprice.sql
+
+with source_data as (
+    -- Select data from the raw source table created by Glue Crawler
+    select
+        series_type,
+        "date",
+        ron95,
+        ron97,
+        diesel,
+        diesel_eastmsia
+    from {{ source('landing_zone', 'fuelprice') }}
+    -- Filter for actual price levels, not weekly changes
+    where series_type = 'level'
+)
+
+select
+    -- Cast data types and rename columns
+    cast("date" as date) as record_date,
+    cast(ron95 as double) as ron95_price_rm,
+    cast(ron97 as double) as ron97_price_rm,
+    cast(diesel as double) as diesel_peninsular_price_rm,
+    cast(diesel_eastmsia as double) as diesel_eastmsia_price_rm
+
+from source_data
