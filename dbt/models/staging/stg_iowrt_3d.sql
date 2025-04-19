050ac22de5a563fc738a0844dbfb6e6875a35288
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-04-20 04:40:19 +0800

    feat: add wholesale 3d stg

---
+++ dbt/models/staging/stg_iowrt_3d.sql
@@ -0,0 +1,23 @@
+-- models/staging/stg_iowrt_3d.sql
+
+with source_data as (
+    -- Select data from the raw source table created by Glue Crawler
+    select
+        series,
+        "date",
+        "group" as group_code, -- Rename 'group' column
+        sales,
+        volume
+    from {{ source('landing_zone', 'iowrt_3d') }}
+    -- Filter for absolute values
+    where series = 'abs'
+)
+
+select
+    -- Cast data types and rename columns
+    cast("date" as date) as record_date,
+    cast(group_code as varchar) as msic_group_code, -- Ensure group code is string
+    cast(sales as double) as sales_value_rm_mil,
+    cast(volume as double) as volume_index
+
+from source_data
