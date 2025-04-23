6424850342c1c17005996fac162cd1d8d4776562
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-04-24 00:09:11 +0800

    fix: use unix timestamp instead of date - athena problematic cast logic

--- dbt/models/staging/stg_iowrt.sql
+++ dbt/models/staging/stg_iowrt.sql
@@ -15,7 +15,7 @@ with source_data as (
 
 select
     -- Cast data types and rename columns
-    cast(from_unixtime(cast("date" as bigint)) as date) as record_date,
+    cast(from_unixtime(cast("date" as bigint)) as timestamp) as record_date,
     cast(sales as double) as sales_value_rm_mil,
     cast(volume as double) as volume_index,
     cast(volume_sa as double) as volume_index_sa
