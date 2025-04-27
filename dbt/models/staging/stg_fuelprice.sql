0a4af7c7a0781b7e403416ae6e0424047b622065
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2025-04-28 02:14:57 +0800

    fix: update date -> ymd_date

--- dbt/models/staging/stg_fuelprice.sql
+++ dbt/models/staging/stg_fuelprice.sql
@@ -4,7 +4,7 @@ with source_data as (
     -- Select data from the raw source table created by Glue Crawler
     select
         series_type,
-        "date",
+        ymd_date, -- Updated column name
         ron95,
         ron97,
         diesel,
@@ -16,7 +16,7 @@ with source_data as (
 
 select
     -- Cast data types and rename columns
-    from_unixtime(cast("date" as bigint)) as record_date,
+    cast(ymd_date as date) as record_date,
     cast(ron95 as double) as ron95_price_rm,
     cast(ron97 as double) as ron97_price_rm,
     cast(diesel as double) as diesel_peninsular_price_rm,
--- dbt/models/staging/stg_iowrt_3d.sql
+++ dbt/models/staging/stg_iowrt_3d.sql
@@ -4,7 +4,7 @@ with source_data as (
     -- Select data from the raw source table created by Glue Crawler
     select
         series,
-        "date",
+        ymd_date, -- Updated column name
         group_code, -- Rename 'group' column
         sales,
         volume
@@ -15,7 +15,7 @@ with source_data as (
 
 select
     -- Cast data types and rename columns
-    cast("date" as timestamp) as record_date,
+    cast(ymd_date as date) as record_date,
     cast(group_code as varchar) as msic_group_code, -- Ensure group code is string
     cast(sales as double) as sales_value_rm_mil,
     cast(volume as double) as volume_index
